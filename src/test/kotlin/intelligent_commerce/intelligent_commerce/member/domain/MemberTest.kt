b7754a72a6f6200c848f5291f71ee0a92ab3af6a
liveforone <yc4852@gmail.com>
2023-06-28 15:21:55 +0900

    Remove unusing dependency.

--- src/test/kotlin/intelligent_commerce/intelligent_commerce/member/domain/MemberTest.kt
+++ src/test/kotlin/intelligent_commerce/intelligent_commerce/member/domain/MemberTest.kt
@@ -4,8 +4,6 @@ import intelligent_commerce.intelligent_commerce.member.domain.util.PasswordUtil
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
-
 class MemberTest {
 
     @Test
