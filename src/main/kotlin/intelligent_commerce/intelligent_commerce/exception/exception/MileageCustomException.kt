51b9068e641143475da6d25ffdfd12c4f4b32e9d
liveforone <yc4852@gmail.com>
2023-06-28 17:15:33 +0900

    Add mileage custom exception.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/exception/exception/MileageCustomException.kt
@@ -0,0 +1,6 @@
+package intelligent_commerce.intelligent_commerce.exception.exception
+
+import java.lang.RuntimeException
+
+class MileageCustomException(message: String) : RuntimeException(message) {
+}
