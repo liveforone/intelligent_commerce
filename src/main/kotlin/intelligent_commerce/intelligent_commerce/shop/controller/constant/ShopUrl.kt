40e30b9c093d998ac7eafda8bed6da9d4969f19d
liveforone <yc4852@gmail.com>
2023-06-29 19:41:16 +0900

    Add shop url.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/shop/controller/constant/ShopUrl.kt
@@ -0,0 +1,9 @@
+package intelligent_commerce.intelligent_commerce.shop.controller.constant
+
+object ShopUrl {
+    const val SHOP_DETAIL = "/shop/{id}"
+    const val SHOP_INFO = "/shop/info"
+    const val CREATE_SHOP = "/shop/create"
+    const val UPDATE_SHOP_NAME = "/shop/update/name"
+    const val UPDATE_SHOP_TEL = "/shop/update/tel"
+}
