6ad60bd43a003bebc8e73cd308c2f9f7f2f0178d
liveforone <yc4852@gmail.com>
2023-06-29 15:46:39 +0900

    Remove entity to dto mapper method.

--- src/main/kotlin/intelligent_commerce/intelligent_commerce/member/dto/response/MemberInfo.kt
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/member/dto/response/MemberInfo.kt
@@ -1,7 +1,6 @@
 package intelligent_commerce.intelligent_commerce.member.dto.response
 
 import intelligent_commerce.intelligent_commerce.member.domain.Address
-import intelligent_commerce.intelligent_commerce.member.domain.Member
 import intelligent_commerce.intelligent_commerce.member.domain.Role
 
 data class MemberInfo(
@@ -9,10 +8,4 @@ data class MemberInfo(
     val bankbookNum: String,
     val auth: Role,
     val address: Address
-) {
-    companion object {
-        fun entityToDto(member: Member): MemberInfo {
-            return MemberInfo(member.id, member.bankbookNum, member.auth, member.address)
-        }
-    }
-}
+)
