b939e0576cd6536d0bd35ef68eb5b07ded250325
liveforone <yc4852@gmail.com>
2023-06-29 17:16:56 +0900

    Refactor rename member is null.

--- src/main/kotlin/intelligent_commerce/intelligent_commerce/exception/message/MemberExceptionMessage.kt
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/exception/message/MemberExceptionMessage.kt
@@ -1,5 +1,5 @@
 package intelligent_commerce.intelligent_commerce.exception.message
 
 enum class MemberExceptionMessage(val status: Int, val message: String) {
-    WRONG_PASSWORD(400, "비밀번호를 틀렸습니다."), MEMBER_Exception_IS_NULL(404, "회원이 존재하지 않습니다.")
+    WRONG_PASSWORD(400, "비밀번호를 틀렸습니다."), MEMBER_IS_NULL(404, "회원이 존재하지 않습니다.")
 }
