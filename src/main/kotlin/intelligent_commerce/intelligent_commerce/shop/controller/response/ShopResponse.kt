4cdb9cbb2aeccfe3c72a0f7925373a541f7e79e9
liveforone <yc4852@gmail.com>
2023-06-29 19:41:32 +0900

    Add shop response.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/shop/controller/response/ShopResponse.kt
@@ -0,0 +1,30 @@
+package intelligent_commerce.intelligent_commerce.shop.controller.response
+
+import intelligent_commerce.intelligent_commerce.shop.dto.response.ShopInfo
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+object ShopResponse {
+
+    private const val CREATE_SHOP_SUCCESS = "상점을 성공적으로 생성하였습니다."
+    private const val UPDATE_SHOP_NAME_SUCCESS = "상호명을 성공적으로 변경하였습니다."
+    private const val UPDATE_TEL = "전화번호를 성공적으로 변경하였습니다."
+
+    fun shopDetailSuccess(shopInfo: ShopInfo): ResponseEntity<*> = ResponseEntity.ok(shopInfo)
+
+    fun shopInfoSuccess(shopInfo: ShopInfo): ResponseEntity<*> = ResponseEntity.ok(shopInfo)
+
+    fun createShopSuccess(): ResponseEntity<*> {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(CREATE_SHOP_SUCCESS)
+    }
+
+    fun updateShopNameSuccess(): ResponseEntity<*> {
+        return ResponseEntity.ok(UPDATE_SHOP_NAME_SUCCESS)
+    }
+
+    fun updateTelSuccess(): ResponseEntity<*> {
+        return ResponseEntity.ok(UPDATE_TEL)
+    }
+}
