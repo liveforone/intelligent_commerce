57c8fdcdabbfeb2eadaeac61674bb160538b3076
liveforone <yc4852@gmail.com>
2023-06-29 14:39:50 +0900

    Add mapper function.

--- src/main/kotlin/intelligent_commerce/intelligent_commerce/mileage/dto/MileageResponse.kt
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/mileage/dto/MileageResponse.kt
@@ -1,14 +1,6 @@
 package intelligent_commerce.intelligent_commerce.mileage.dto
 
-import intelligent_commerce.intelligent_commerce.mileage.domain.Mileage
-
 data class MileageResponse(
     val id: Long?,
     val mileagePoint: Long
-) {
-    companion object {
-        fun entityToDto(mileage: Mileage): MileageResponse {
-            return MileageResponse(mileage.id, mileage.mileagePoint)
-        }
-    }
-}
+)
