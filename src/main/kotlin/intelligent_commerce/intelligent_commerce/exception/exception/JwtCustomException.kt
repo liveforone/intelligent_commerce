8bdade24b1f655a91ed275840f5dec963cf2f1ce
liveforone <yc4852@gmail.com>
2023-06-29 16:48:35 +0900

    Refactor param name to jwtExceptionMessage.

--- src/main/kotlin/intelligent_commerce/intelligent_commerce/exception/exception/JwtCustomException.kt
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/exception/exception/JwtCustomException.kt
@@ -3,5 +3,5 @@ package intelligent_commerce.intelligent_commerce.exception.exception
 import intelligent_commerce.intelligent_commerce.exception.message.JwtExceptionMessage
 import java.lang.RuntimeException
 
-class JwtCustomException(val jwtExceptionMessage: JwtExceptionMessage) : RuntimeException(jwtMessage.message) {
+class JwtCustomException(val jwtExceptionMessage: JwtExceptionMessage) : RuntimeException(jwtExceptionMessage.message) {
 }
