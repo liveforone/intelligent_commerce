a4c49db16ccb4fd320d8c7cb81b655284a6f4942
liveforone <yc4852@gmail.com>
2023-06-29 19:41:42 +0900

    Add shop controller log.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/shop/controller/constant/ShopControllerLog.kt
@@ -0,0 +1,7 @@
+package intelligent_commerce.intelligent_commerce.shop.controller.constant
+
+enum class ShopControllerLog(val log: String) {
+    CREATE_SHOP_SUCCESS("상점 생성 성공"),
+    UPDATE_SHOP_NAME_SUCCESS("상호명 변경 성공"),
+    UPDATE_TEL_SUCCESS("전화번호 변경 성공")
+}
