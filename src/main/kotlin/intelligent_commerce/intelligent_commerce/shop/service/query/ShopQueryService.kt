1ca30b0da4d76ab93515cbdc2ab97f64ae501301
liveforone <yc4852@gmail.com>
2023-06-29 19:40:49 +0900

    Add shop query service.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/shop/service/query/ShopQueryService.kt
@@ -0,0 +1,18 @@
+package intelligent_commerce.intelligent_commerce.shop.service.query
+
+import intelligent_commerce.intelligent_commerce.shop.dto.response.ShopInfo
+import intelligent_commerce.intelligent_commerce.shop.repository.ShopRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class ShopQueryService @Autowired constructor(
+    private val shopRepository: ShopRepository
+) {
+
+    fun getShopById(id: Long): ShopInfo = shopRepository.findOneDtoById(id)
+
+    fun getShopByIdentity(identity: String): ShopInfo = shopRepository.findOneDtoByIdentity(identity)
+}
