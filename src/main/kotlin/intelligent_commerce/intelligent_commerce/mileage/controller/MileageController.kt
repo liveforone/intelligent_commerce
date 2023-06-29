6cdbbc9d4bef94f3c7229322973496245cc75771
liveforone <yc4852@gmail.com>
2023-06-29 14:42:19 +0900

    Add mileage controller.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/mileage/controller/MileageController.kt
@@ -0,0 +1,21 @@
+package intelligent_commerce.intelligent_commerce.mileage.controller
+
+import intelligent_commerce.intelligent_commerce.mileage.controller.constant.MileageUrl
+import intelligent_commerce.intelligent_commerce.mileage.service.query.MileageQueryService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.security.Principal
+
+@RestController
+class MileageController @Autowired constructor(
+    private val mileageQueryService: MileageQueryService,
+) {
+
+    @GetMapping(MileageUrl.MILEAGE_INFO)
+    fun mileageInfo(principal: Principal): ResponseEntity<*> {
+        val mileage = mileageQueryService.getMileageByMemberIdentity(identity = principal.name)
+        return ResponseEntity.ok(mileage)
+    }
+}
