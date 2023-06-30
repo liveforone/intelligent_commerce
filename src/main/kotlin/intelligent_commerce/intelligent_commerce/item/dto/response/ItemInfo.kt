fa163846b25936b7bd84ef640cdedeef752f2c6e
liveforone <yc4852@gmail.com>
2023-06-30 22:40:15 +0900

    Add item info dto.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/item/dto/response/ItemInfo.kt
@@ -0,0 +1,10 @@
+package intelligent_commerce.intelligent_commerce.item.dto.response
+
+data class ItemInfo(
+    val id: Long?,
+    val shopId: Long,
+    val title: String,
+    val content: String,
+    val price: ULong,
+    val remaining: ULong
+)
