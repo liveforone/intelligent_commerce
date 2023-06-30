a69172edb7302638f80e2b2f58ff09ef07d07556
liveforone <yc4852@gmail.com>
2023-06-30 22:39:20 +0900

    Add nullable false constrains in field.

--- src/main/kotlin/intelligent_commerce/intelligent_commerce/member/domain/Address.kt
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/member/domain/Address.kt
@@ -1,10 +1,14 @@
 package intelligent_commerce.intelligent_commerce.member.domain
 
+import jakarta.persistence.Column
 import jakarta.persistence.Embeddable
 
 @Embeddable
 data class Address(
+    @Column(nullable = false)
     val city: String,
+    @Column(nullable = false)
     val roadNum: String,
+    @Column(nullable = false)
     val detail: String
 )
