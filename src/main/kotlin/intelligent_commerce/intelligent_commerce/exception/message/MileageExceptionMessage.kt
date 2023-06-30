fbb1227ff4f12403f3d21c53b8d6b3b4f912f498
liveforone <yc4852@gmail.com>
2023-06-30 21:16:58 +0900

    Refactor editor error for name.

--- src/main/kotlin/intelligent_commerce/intelligent_commerce/exception/message/MileageExceptionMessage.kt
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/exception/message/MileageExceptionMessage.kt
@@ -1,5 +1,5 @@
 package intelligent_commerce.intelligent_commerce.exception.message
 
 enum class MileageExceptionMessage(val status: Int, val message: String) {
-    MILEAGE_Exception_IS_NULL(404, "마일리지가 존재하지 않습니다.")
+    MILEAGE_IS_NULL(404, "마일리지가 존재하지 않습니다.")
 }
