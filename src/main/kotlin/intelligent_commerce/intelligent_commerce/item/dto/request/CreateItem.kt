2fe7c4194885772ca14cd5c460fe8df8a86fd104
liveforone <yc4852@gmail.com>
2023-06-30 22:39:57 +0900

    Add create item dto.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/item/dto/request/CreateItem.kt
@@ -0,0 +1,15 @@
+package intelligent_commerce.intelligent_commerce.item.dto.request
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Positive
+
+data class CreateItem(
+    @field:NotBlank(message = "상품명을 입력하세요.")
+    var title: String?,
+    @field:NotBlank(message = "상품 설명을 입력하세요.")
+    var content: String?,
+    @field:Positive(message = "상품가격은 양수만 가능합니다.")
+    var price: ULong?,
+    @field:Positive(message = "재고는 양수만 가능합니다.")
+    var remaining: ULong?
+)
