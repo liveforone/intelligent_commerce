b251a655342e04f0633838591f2b3bfdae1fdd73
liveforone <yc4852@gmail.com>
2023-07-01 15:36:11 +0900

    Add item param.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/item/controller/constant/ItemParam.kt
@@ -0,0 +1,9 @@
+package intelligent_commerce.intelligent_commerce.item.controller.constant
+
+object ItemParam {
+    const val ID = "id"
+    const val SHOP_ID = "shopId"
+    const val LAST_ID = "lastId"
+    const val KEYWORD = "keyword"
+    const val DEFAULT_VALUE = "0"
+}
