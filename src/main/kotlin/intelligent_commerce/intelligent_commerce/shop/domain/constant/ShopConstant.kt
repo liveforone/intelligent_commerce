a97d304c55fe7fd59f80049205117c665e5fa782
liveforone <yc4852@gmail.com>
2023-07-01 14:02:06 +0900

    Add shop constant.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/shop/domain/constant/ShopConstant.kt
@@ -0,0 +1,6 @@
+package intelligent_commerce.intelligent_commerce.shop.domain.constant
+
+object ShopConstant {
+    const val SELLER_COLUMN_NAME = "seller_identity"
+    const val IDENTITY = "identity"
+}
