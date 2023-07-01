36311a4c5ca304852a7997ad2c3ccd12ed5137a7
liveforone <yc4852@gmail.com>
2023-07-01 15:36:18 +0900

    Add item url.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/item/controller/constant/ItemUrl.kt
@@ -0,0 +1,14 @@
+package intelligent_commerce.intelligent_commerce.item.controller.constant
+
+object ItemUrl {
+    const val ITEM_DETAIL = "/item/detail/{id}"
+    const val ITEM_HOME = "/item/home"
+    const val SHOP_ITEMS = "/item/shop/{shopId}"
+    const val SEARCH_ITEM = "/item/search"
+    const val CREATE_ITEM = "/item/create"
+    const val UPDATE_TITLE = "/item/update/title"
+    const val UPDATE_CONTENT = "/item/update/content"
+    const val UPDATE_PRICE = "/item/update/price"
+    const val ADD_REMAINING = "/item/update/add-remaining"
+    const val DELETE_ITEM = "/item/delete/{id}"
+}
