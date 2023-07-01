df331d76b3b2ddc3a648e78208585afb35d63eae
liveforone <yc4852@gmail.com>
2023-07-01 14:01:59 +0900

    Add mileage constant.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/mileage/domain/constant/MileageConstant.kt
@@ -0,0 +1,7 @@
+package intelligent_commerce.intelligent_commerce.mileage.domain.constant
+
+object MileageConstant {
+    const val MEMBER_COLUMN_NAME = "member_identity"
+    const val IDENTITY = "identity"
+    const val USE_MILEAGE_LIMIT_POINT: Long = 5000
+}
