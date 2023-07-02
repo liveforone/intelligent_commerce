eaf8799d9bbcb937222c10bb295f80db2e7ae93f
liveforone <yc4852@gmail.com>
2023-07-03 00:01:03 +0900

    Add delivery charege.

--- src/main/kotlin/intelligent_commerce/intelligent_commerce/item/dto/request/CreateItem.kt
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/item/dto/request/CreateItem.kt
@@ -2,6 +2,7 @@ package intelligent_commerce.intelligent_commerce.item.dto.request
 
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Positive
+import jakarta.validation.constraints.PositiveOrZero
 
 data class CreateItem(
     @field:NotBlank(message = "상품명을 입력하세요.")
@@ -10,6 +11,8 @@ data class CreateItem(
     var content: String?,
     @field:Positive(message = "상품가격은 양수만 가능합니다.")
     var price: Long?,
+    @field:PositiveOrZero(message = "배송비는 양수 혹은 0만 가능합니다.")
+    var deliveryCharge: Long?,
     @field:Positive(message = "재고는 양수만 가능합니다.")
     var remaining: Long?
 )
