574689599558c6e89107513261c8d745e6460a52
liveforone <yc4852@gmail.com>
2023-07-03 15:00:43 +0900

    Add order constant.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/order/domain/constant/OrdersConstant.kt
@@ -0,0 +1,9 @@
+package intelligent_commerce.intelligent_commerce.order.domain.constant
+
+object OrdersConstant {
+    const val MEMBER_COLUMN_NAME = "member_identity"
+    const val IDENTITY = "identity"
+    const val BASIC_QUANTITY: Long = 1
+    const val BASIC_MILEAGE: Long = 0
+    const val LIMIT_CANCEL_DAY: Long = 7
+}
