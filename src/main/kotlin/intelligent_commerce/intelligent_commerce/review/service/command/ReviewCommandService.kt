1c1e522c2ea37af12a7386494f1ce3721cf2cbf5
liveforone <yc4852@gmail.com>
2023-07-03 17:49:49 +0900

    Add review command service.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/review/service/command/ReviewCommandService.kt
@@ -0,0 +1,34 @@
+package intelligent_commerce.intelligent_commerce.review.service.command
+
+import intelligent_commerce.intelligent_commerce.exception.exception.ReviewException
+import intelligent_commerce.intelligent_commerce.exception.message.ReviewExceptionMessage
+import intelligent_commerce.intelligent_commerce.order.repository.OrdersRepository
+import intelligent_commerce.intelligent_commerce.review.domain.Review
+import intelligent_commerce.intelligent_commerce.review.dto.request.CreateReview
+import intelligent_commerce.intelligent_commerce.review.repository.ReviewRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional
+class ReviewCommandService @Autowired constructor(
+    private val reviewRepository: ReviewRepository,
+    private val ordersRepository: OrdersRepository
+) {
+
+    fun createReview(requestDto: CreateReview) {
+        Review.create(ordersRepository.findOneById(requestDto.orderId!!), requestDto.content!!)
+            .also {
+                reviewRepository.save(it)
+            }
+    }
+
+    fun deleteReview(id: Long, identity: String) {
+        reviewRepository.findOneByIdJoinMember(id)
+            .also {
+                if (!it.isOwnerOfReview(identity)) throw ReviewException(ReviewExceptionMessage.NOT_OWNER_OF_REVIEW)
+                reviewRepository.delete(it)
+            }
+    }
+}
