7a33ffc9964f2ad51180046bd9d8d0d751fd0f46
liveforone <yc4852@gmail.com>
2023-07-03 15:02:49 +0900

    Add order param.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/order/controller/constant/OrderParam.kt
@@ -0,0 +1,7 @@
+package intelligent_commerce.intelligent_commerce.order.controller.constant
+
+object OrderParam {
+    const val ID = "id"
+    const val LAST_ID = "lastId"
+    const val DEFAULT_LAST_ID = "0"
+}
