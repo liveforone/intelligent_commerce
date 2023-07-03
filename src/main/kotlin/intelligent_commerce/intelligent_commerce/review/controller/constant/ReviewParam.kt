158067301dfa09ea15e2f17139e02aa00537db6c
liveforone <yc4852@gmail.com>
2023-07-03 17:50:16 +0900

    Add review param.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/review/controller/constant/ReviewParam.kt
@@ -0,0 +1,9 @@
+package intelligent_commerce.intelligent_commerce.review.controller.constant
+
+object ReviewParam {
+    const val ID = "id"
+    const val ORDER_ID = "orderId"
+    const val ITEM_ID = "itemId"
+    const val LAST_ID = "lastId"
+    const val DEFAULT_LAST_ID = "0"
+}
