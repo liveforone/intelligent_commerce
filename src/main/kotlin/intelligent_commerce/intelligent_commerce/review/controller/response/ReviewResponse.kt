22979e1edd5c131f7d5a7d3d656fe9a7af58c1ad
liveforone <yc4852@gmail.com>
2023-07-03 17:50:38 +0900

    Add review response.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/review/controller/response/ReviewResponse.kt
@@ -0,0 +1,33 @@
+package intelligent_commerce.intelligent_commerce.review.controller.response
+
+import intelligent_commerce.intelligent_commerce.review.dto.response.ReviewInfo
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+object ReviewResponse {
+
+    private const val CREATE_REVIEW_SUCCESS = "리뷰를 성공적으로 등록했습니다."
+    private const val DELETE_REVIEW_SUCCESS = "리뷰를 성공적으로 삭제했습니다."
+
+    fun reviewDetailSuccess(reviewInfo: ReviewInfo): ResponseEntity<ReviewInfo> {
+        return ResponseEntity.ok(reviewInfo)
+    }
+
+    fun reviewByOrderSuccess(reviewInfo: ReviewInfo): ResponseEntity<ReviewInfo> {
+        return ResponseEntity.ok(reviewInfo)
+    }
+
+    fun reviewsByItemSuccess(reviews: List<ReviewInfo>): ResponseEntity<List<ReviewInfo>> {
+        return ResponseEntity.ok(reviews)
+    }
+
+    fun createReviewSuccess(): ResponseEntity<String> {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(CREATE_REVIEW_SUCCESS)
+    }
+
+    fun deleteReviewSuccess(): ResponseEntity<String> {
+        return ResponseEntity.ok(DELETE_REVIEW_SUCCESS)
+    }
+}
