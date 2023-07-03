7fe8e948b30bb66942074fafab8aa04b1a4fa541
liveforone <yc4852@gmail.com>
2023-07-03 17:48:22 +0900

    Add review.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/review/domain/Review.kt
@@ -0,0 +1,29 @@
+package intelligent_commerce.intelligent_commerce.review.domain
+
+import intelligent_commerce.intelligent_commerce.order.domain.Orders
+import jakarta.persistence.*
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@Entity
+@EntityListeners(AuditingEntityListener::class)
+class Review private constructor(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long?,
+    @OneToOne(fetch = FetchType.LAZY) @JoinColumn(
+        unique = true,
+        updatable = false
+    ) val order: Orders,
+    @Column(nullable = false) val content: String,
+    @Column(nullable = false, updatable = false) val createdDate: LocalDateTime = LocalDateTime.now()
+) {
+
+    companion object {
+        fun create(order: Orders, content: String): Review {
+            return Review(null, order, content)
+        }
+    }
+
+    fun isOwnerOfReview(identity: String): Boolean{
+        return order.member.identity == identity
+    }
+}
