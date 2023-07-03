276e9cda4893f54a53b6e0cb82b79ab7576fa56f
liveforone <yc4852@gmail.com>
2023-07-03 17:50:24 +0900

    Add review controller log.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/review/controller/constant/ReviewControllerLog.kt
@@ -0,0 +1,6 @@
+package intelligent_commerce.intelligent_commerce.review.controller.constant
+
+enum class ReviewControllerLog(val log: String) {
+    CREATE_REVIEW_SUCCESS("리뷰 등록 성공"),
+    DELETE_REVIEW_SUCCESS("리뷰 삭제 성공")
+}
