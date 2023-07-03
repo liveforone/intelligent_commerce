01e6826e579b1c5b0df5535fd45cdd882313c8a2
liveforone <yc4852@gmail.com>
2023-07-03 15:02:55 +0900

    Add order url.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/order/controller/constant/OrderUrl.kt
@@ -0,0 +1,10 @@
+package intelligent_commerce.intelligent_commerce.order.controller.constant
+
+object OrderUrl {
+    const val ORDER_DETAIL = "/order/detail/{id}"
+    const val MY_ORDER = "/order/my-order"
+    const val SHOP_ORDER_LIST = "/order/shop/list"
+    const val ORDER = "/order"
+    const val DELIVERY_COMPLETED = "/order/delivery-completed/{id}"
+    const val ORDER_CANCEL = "/order/cancel/{id}"
+}
