ded3b5237edefc1c59ec8dc0f33fb963bc711d09
liveforone <yc4852@gmail.com>
2023-07-03 17:50:10 +0900

    Add review url.

---
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/review/controller/constant/ReviewUrl.kt
@@ -0,0 +1,9 @@
+package intelligent_commerce.intelligent_commerce.review.controller.constant
+
+object ReviewUrl {
+    const val REVIEW_DETAIL = "/review/detail/{id}"
+    const val REVIEW_BY_ORDER = "/review/order/{orderId}"
+    const val REVIEWS_BY_ITEM = "/review/item/{itemId}"
+    const val CREATE_REVIEW = "/review/create"
+    const val DELETE_REVIEW = "/review/delete/{id}"
+}
