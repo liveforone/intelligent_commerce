f46a5c8bda9871e7184a2224b017155ea1bba859
liveforone <yc4852@gmail.com>
2023-07-05 15:25:29 +0900

    Refactor enum value open for visibility.

--- src/main/kotlin/intelligent_commerce/intelligent_commerce/exception/message/ShopExceptionMessage.kt
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/exception/message/ShopExceptionMessage.kt
@@ -1,5 +1,6 @@
 package intelligent_commerce.intelligent_commerce.exception.message
 
 enum class ShopExceptionMessage(val status: Int, val message: String) {
-    SHOP_IS_NULL(404, "상점이 존재하지 않습니다."), AUTH_IS_NOT_SELLER(403, "접근 불가능한 권한입니다.")
+    SHOP_IS_NULL(404, "상점이 존재하지 않습니다."),
+    AUTH_IS_NOT_SELLER(403, "접근 불가능한 권한입니다.")
 }
