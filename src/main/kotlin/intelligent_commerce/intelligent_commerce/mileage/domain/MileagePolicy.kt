62b711252e3d40d4c0535474957c858d89a83ec7
liveforone <yc4852@gmail.com>
2023-07-05 15:29:50 +0900

    Refactor short method change to single method.

--- src/main/kotlin/intelligent_commerce/intelligent_commerce/mileage/domain/MileagePolicy.kt
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/mileage/domain/MileagePolicy.kt
@@ -3,7 +3,5 @@ package intelligent_commerce.intelligent_commerce.mileage.domain
 object MileagePolicy {
     private const val MILEAGE_POLICY = 0.01
 
-    fun calculateMileage(itemPrice: Long): Long {
-        return (itemPrice * MILEAGE_POLICY).toLong()
-    }
+    fun calculateMileage(itemPrice: Long): Long = (itemPrice * MILEAGE_POLICY).toLong()
 }
