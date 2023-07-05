592067bf2421b3adb2442710668c2f0d9ca38ebb
liveforone <yc4852@gmail.com>
2023-07-05 15:26:59 +0900

    Refactor enum value open for visibility.

--- src/main/kotlin/intelligent_commerce/intelligent_commerce/member/domain/Role.kt
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/member/domain/Role.kt
@@ -1,5 +1,7 @@
 package intelligent_commerce.intelligent_commerce.member.domain
 
 enum class Role(val auth:String) {
-    MEMBER("ROLE_MEMBER"), SELLER("ROLE_SELLER"), ADMIN("ROLE_ADMIN")
+    MEMBER("ROLE_MEMBER"),
+    SELLER("ROLE_SELLER"),
+    ADMIN("ROLE_ADMIN")
 }
--- src/main/kotlin/intelligent_commerce/intelligent_commerce/order/domain/OrderState.kt
+++ src/main/kotlin/intelligent_commerce/intelligent_commerce/order/domain/OrderState.kt
@@ -1,5 +1,7 @@
 package intelligent_commerce.intelligent_commerce.order.domain
 
 enum class OrderState {
-    DELIVERY_READY_AND_ING, DELIVERY_COMPLETED, CANCEL
+    DELIVERY_READY_AND_ING,
+    DELIVERY_COMPLETED,
+    CANCEL
 }
